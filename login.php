4d0c779819f13433d7cee30b30b4615f3a043e35
AliHuseyno <149154138+AliHuseyno@users.noreply.github.com>
2024-05-15 23:44:52 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+
+$eposta = "g201210090@sakarya.edu.tr";
+$sifre = "g201210090";
+
+
+if(($_POST['emailgiris']==$eposta) && ($_POST['sifregiris']==$sifre) )
+{
+
+    echo " <h1> HOŞGELDİN,</h1>";
+    echo " <h1><i> g201210090</i>";
+    echo "<h2><a  href ='index.html'>Ana Sayfa</a></h2>";
+
+    
+}
+else{
+    
+
+    echo "<h1> HATALI GİRİŞ</h1>";
+    echo "<h2><a href ='login.html'>Geri Dön</a></h2>";
+   
+
+}
+
+
+
+?>
